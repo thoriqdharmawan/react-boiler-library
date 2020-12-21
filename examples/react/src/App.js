59e91af60fbdf1adcbb9868fa0b1d58914b8d384
thoriqdharmawan <thoriqd21@gmail.com>
2020-12-21 19:10:03 +0700

    initial commit

---
+++ examples/react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, Button } from 'react-ui'
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Text>Hi Text Component</Text>
+            <Button>Click me</Button>
+            <Button >Click me 2</Button>
+        </React.Fragment>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Text from './components/text/Text'
+import Button from './components/button/Button'
+import Modal from './components/modal/Modal'
+export { Text, Button, Modal }
